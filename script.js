5ceb3784213e7024ef655b51361ee87a33b5590a
function07 <houssine199222@gmail.com>
2022-09-11 22:28:44 -0400

    Add function to allow drawing and tweak code to reduce repetitions

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
+const btn1 = document.querySelector('#btn1');
+
+const btn2 = document.querySelector('#btn2');
+
+const btn3 = document.querySelector('#btn3');
+
 //Creates a grid with the size indicated by gridSize
 function createGrid (gridSize){
     for(let i = 0; i < gridSize; i++){
         const container = document.querySelector('#container');
-
         const grid = document.createElement('div');
         grid.setAttribute('id', 'grid');
         container.appendChild(grid);
          };
 };
 
-const btn1 = document.querySelector('#btn1');
-
-btn1.addEventListener('click', (e) => {
+// Clears grid
+function clear(){
     const container = document.querySelector('#container');
         while(container.firstChild){
             container.removeChild(container.firstChild);
         };
+};
+// Allows 'drawing'
+function draw(gridSelect){
+    const grids = document.querySelectorAll('#grid')    
+    grids.forEach((grid) => {
+        grid.classList.add(gridSelect);
+        grid.addEventListener('mouseover', (e) => {
+            e.target.style.cssText = 'background-color: black;'
+        });
+    });
+};
+
+btn1.addEventListener('click', () => {
+
+    clear();
+    
     createGrid(256);
-    const grids = document.querySelectorAll('#grid')
-    grids.forEach((grids) => {
-        grids.style.height = '31.25px';
-        grids.style.width = '31.25px';
-    });
+    
+    draw('grid1')
 });
 
-const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
 
-btn2.addEventListener('click', (e) => {
-    const container = document.querySelector('#container');
-        while(container.firstChild){
-            container.removeChild(container.firstChild);
-        };
+    clear();
+    
     createGrid(1024);
-    const grids = document.querySelectorAll('#grid')
-    grids.forEach((grids) => {
-        grids.style.height = '15.625px';
-        grids.style.width = '15.625px';
-    });
+    
+    draw('grid2')
 });
 
-const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
 
-btn3.addEventListener('click', (e) => {
-    const container = document.querySelector('#container');
-        while(container.firstChild){
-            container.removeChild(container.firstChild);
-        };
+    clear();
+    
     createGrid(4096);
-    const grids = document.querySelectorAll('#grid')
-    grids.forEach((grids) => {
-        grids.style.height = '7.8125px';
-        grids.style.width = '7.8125px';
-    });
+    
+    draw('grid3')
 });
+
